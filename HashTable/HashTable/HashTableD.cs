90262389545f2bfed992bdd95f092ffa829a2c48
Justas-Milisiunas <justas.milisiunas@gmail.com>
2019-03-23 19:59:38 +0200

    Added hashtable data structure using disk memory

---
+++ HashTable/HashTable/HashTableD.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace HashTable
+{
+    class HashTableD
+    {
+        private readonly double loadFactor = 0.75;
+        private FileStream fs { get; set; }
+
+        private string fileName;
+        private int size;
+        private int capacity;
+
+        private int keySize = 8;
+        private int chainsCount;
+
+
+        public HashTableD(int capacity = 10, int keySize = 8,string fileName = "hashfile.dat")
+        {
+            this.capacity = capacity;
+            this.size = 0;
+            this.chainsCount = 0;
+            this.fileName = fileName;
+            this.keySize = keySize;
+
+            if (File.Exists(this.fileName))
+                File.Delete(this.fileName);
+
+            this.fs = new FileStream(this.fileName, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            ResetTable();
+        }
+
+        public double Put(string key, double value)
+        {
+            if (key == null)
+            {
+                throw new ArgumentException("Key or value is null in Put(string key, double value)");
+            }
+
+            bool keyFound;
+            int foundIndex = FindPosition(key, out keyFound);
+            if(foundIndex == -1)
+            {
+                Rehash(capacity * 2);
+                Put(key, value);
+            }
+            else
+            {
+                Byte[] data = new Byte[8 + keySize];
+                data = Encoding.UTF8.GetBytes(key);
+
+                fs.Seek(foundIndex * (8 + keySize), SeekOrigin.Begin);
+                fs.Write(data, 0, keySize);
+
+                data = BitConverter.GetBytes(value);
+                fs.Write(data, 0, 8);
+
+                if(keyFound == false)
+                    size++;
+
+                if (size > capacity * loadFactor)
+                    Rehash(capacity * 2);
+            }
+
+            return value;
+        }
+
+        public double? Get(string key)
+        {
+            if (key == null)
+                throw new ArgumentNullException("Key is null in Get(string key)");
+
+            int index = Hash(key);
+            int tempIndex = index;
+            int j = 0;
+
+            for (int i = 0; i < capacity; i++)
+            {
+                Byte[] data = new Byte[8 + keySize];
+                fs.Seek(index * (8 + keySize), SeekOrigin.Begin);
+                fs.Read(data, 0, 8 + keySize);
+
+                string tempKey = Encoding.UTF8.GetString(data, 0, keySize);
+                double tempValue = BitConverter.ToDouble(data, keySize);
+
+                if (tempKey == "\0\0\0\0\0\0\0\0")
+                    return null;
+                if (tempKey.Equals(key))
+                    return tempValue;
+                j++;
+                index = (tempIndex + j) % capacity;
+            }
+            return null;
+        }
+
+        public bool Contains(string key)
+        {
+            return Get(key) == null ? false : true;
+        }
+
+        private int FindPosition(string key, out bool keyFound)
+        {
+            if (key == null)
+                throw new ArgumentNullException("Key is null");
+
+            keyFound = false;
+            int index = Hash(key);
+            int tempIndex = index;
+            int i = 0;
+
+            for (int j = 0; j < capacity; j++)
+            {
+                Byte[] data = new Byte[8 + keySize];
+                fs.Seek(index * (8 + keySize), SeekOrigin.Begin);
+                fs.Read(data, 0, 8 + keySize);
+                string tempKey = Encoding.UTF8.GetString(data, 0, 8);
+                double tempValue = BitConverter.ToDouble(data, keySize);
+
+                if (tempKey.Equals(key))
+                {
+                    keyFound = true;
+                    return index;
+                }
+
+                if(tempKey == "\0\0\0\0\0\0\0\0")
+                {
+                    return index;
+                }
+
+                i++;
+                index = (tempIndex + i) % capacity;
+            }
+
+            return -1;
+        }
+
+        private void Rehash(int capacity)
+        {
+            HashTableD newHT = new HashTableD(capacity, keySize, "rehashed" + System.DateTime.UtcNow.Ticks + ".dat");
+            for(int i = 0; i < capacity; i++)
+            {
+                Byte[] data = new Byte[8 + keySize];
+                fs.Seek(i * (8 + keySize), SeekOrigin.Begin);
+                fs.Read(data, 0, 8 + keySize);
+                string key = Encoding.UTF8.GetString(data, 0, keySize);
+                double value = BitConverter.ToDouble(data, keySize);
+
+                if(key != "\0\0\0\0\0\0\0\0")
+                {
+                    newHT.Put(key, value);
+                }
+            }
+            this.fs.Close();
+            File.Delete(this.fileName);
+
+            this.fileName = newHT.fileName;
+            this.fs = newHT.fs;
+            this.chainsCount = newHT.chainsCount;
+            this.capacity = newHT.capacity;
+            this.size = newHT.size;
+        }
+
+        private void ResetTable()
+        {
+            for(int i = 0; i < capacity * (8 + keySize); i++)
+            {
+                //Byte[] data = new Byte[4];
+                //data = BitConverter.GetBytes(-1);
+                //fs.Seek(i * 4, SeekOrigin.Begin);
+                //fs.Write(data, 0, 4);
+                fs.WriteByte(0);
+            }
+            
+        }
+
+        private int Hash(string key)
+        {
+            return Math.Abs(key.GetHashCode()) % capacity;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder output = new StringBuilder();
+            for (int i = 0; i < capacity; i++)
+            {
+                Byte[] data = new Byte[8 + keySize];
+                fs.Seek(i * (8 + keySize), SeekOrigin.Begin);
+                fs.Read(data, 0, 8 + keySize);
+                string key = Encoding.UTF8.GetString(data, 0, keySize);
+                double value = BitConverter.ToDouble(data, keySize);
+
+                output.Append(string.Format("[{0}] ->", i));
+
+                if(key != "\0\0\0\0\0\0\0\0")
+                    output.Append(string.Format("{0} {1} [{2}]", key, value, Hash(key)));
+                output.Append("\n");
+            }
+
+            return output.ToString();
+        }
+
+        public string FileName => fileName;
+        protected class Entry
+        {
+            public string key;
+            public double value;
+
+            public Entry() { }
+
+            public Entry(string key, double value)
+            {
+                this.key = key;
+                this.value = value;
+            }
+        }
+    }
+}
--- HashTable/HashTable/Program.cs
+++ HashTable/HashTable/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
 
 namespace HashTable
 {
@@ -10,19 +12,112 @@ namespace HashTable
     {
         static void Main(string[] args)
         {
-            //HashTable tmp = new HashTable(5);
-            HashTable lentele = new HashTable();
-            lentele.Put("10", 55);
+            int n = 10000000;
+            int seed = 121;
+            string dataFile = "data-file.txt";
+            string[] keys = new string[n];
 
-            //Random rnd = new Random(100);
-            //for(int i = 0; i < 20; i++)
-            //{
-            //    tmp.Put(System.DateTime.Now.ToString(), rnd.NextDouble());
-            //}
-
-            //Console.WriteLine(tmp.ToString());
+            keys = GenerateDataFile(dataFile, n, seed);
+            TestHashTable_D(dataFile, n, seed, keys);
+            //TestHashTable_OP(dataFile, n, seed, keys);
 
             Console.ReadKey();
+        }
+
+        public static void TestHashTable_OP(string dataFile, int n, int seed, string[] keys)
+        {
+            //Reads data from file
+            HashTable lentele = new HashTable();
+            using (StreamReader reader = new StreamReader(dataFile))
+            {
+                int i = 0;
+                string line = null;
+                while((line = reader.ReadLine()) != null)
+                {
+                    string[] words = line.Split(' ');
+                    lentele.Put(words[0], double.Parse(words[1]));
+                    keys[i++] = words[0];
+                }
+            }
+
+            int sum = 0;
+            Stopwatch t1 = new Stopwatch();
+            t1.Start();
+            for (int i = 0; i < n; i++)
+            {
+                if (lentele.Contains(keys[i]))
+                    sum++;
+            }
+            t1.Stop();
+
+            Console.WriteLine(sum == n ? "Rado visus" : "Visu nerado");
+            Console.WriteLine("Uztruko: " + t1.Elapsed);
+
+            lentele = null;
+            keys = null;
+            GC.Collect();
+        }
+
+        public static void TestHashTable_D(string dataFile, int n, int seed, string[] keys)
+        {
+            HashTableD table = new HashTableD();
+            ReadData(dataFile, table);
+
+            int sum = 0;
+            for(int i = 0; i < n; i++)
+            {
+                if (table.Contains(keys[i]))
+                    sum++;
+            }
+
+            Console.WriteLine(table.ToString());
+            Console.WriteLine("Rasta: " + sum);
+
+            while(true)
+            {
+                string input = Console.ReadLine();
+                Console.WriteLine("Value: " + table.Get(input));
+            }
+        }
+
+        public static void ReadData(string dataFile, HashTableD table)
+        {
+            using (StreamReader reader = new StreamReader(dataFile))
+            {
+                string line = null;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] words = line.Split(' ');
+                    table.Put(words[0], double.Parse(words[1]));
+                }
+            }
+        }
+
+        public static string[] GenerateDataFile(string fileName, int n, int seed)
+        {
+            string[] keys = new string[n];
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                Random rand = new Random(seed);
+                //writer.WriteLine(12345678 + " " + rand.NextDouble());
+                //writer.WriteLine(11345678 + " " + rand.NextDouble());
+                //writer.WriteLine(11145678 + " " + rand.NextDouble());
+                //writer.WriteLine(11115678 + " " + rand.NextDouble());
+                //writer.WriteLine(11115678 + " " + 111);
+                //writer.WriteLine(11111678 + " " + rand.NextDouble());
+                //writer.WriteLine(11111678 + " " + rand.NextDouble());
+                //writer.WriteLine(11111678 + " " + rand.NextDouble());
+                //writer.WriteLine(11111678 + " " + rand.NextDouble());
+                //writer.WriteLine(11111678 + " " + rand.NextDouble());
+                for (int i = 0; i < n; i++)
+                {
+                    string key = rand.Next(10000000, 99999999).ToString();
+                    keys[i] = key;
+                    writer.WriteLine(key + " " + rand.NextDouble());
+                }
+            }
+
+            return keys;
         }
     }
 }
